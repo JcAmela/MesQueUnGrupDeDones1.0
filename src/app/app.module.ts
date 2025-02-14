a037db01189c9a657c18480e5f2377baf0077be9
JcAmela <juancarlosamela@gmail.com>
2025-02-14 17:50:26 +0100

    importando el icono user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-import { LucideAngularModule, Loader } from 'lucide-angular';
+import { LucideAngularModule, Loader, User } from 'lucide-angular'; // Importa el ícono User
 import { ActivitiesModule } from './features/activities/activities.module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -20,7 +20,7 @@ import { AppComponent } from './app.component';
     ActivitiesModule,
     MatIconModule,
     MatSnackBarModule,
-    LucideAngularModule.pick({ Loader })
+    LucideAngularModule.pick({ Loader, User }) // Incluye el ícono User
   ],
   providers: [],
   bootstrap: [AppComponent]
